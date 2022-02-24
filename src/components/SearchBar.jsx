3cc11eba2e1fbd2a07db4b5e5a4b1384ad993c12
Rohit <rohitkumbhar3456@gmail.com>
2022-02-24 12:29:40 +0530

    fetch url complete

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,55 @@
 import React , {useState} from "react";
 import { FaSistrix , FaRandom } from "react-icons/fa";
+import Image from "./Image";
 import "./searchBar.css";
 
 export default function SearchBar(){
-
+      const [value , setValue]=useState("");
+      const [result,setResult]=useState(null);
+      const [loading , setLoading] = useState();
+      function fet(){
+        if(!value)return;
+        async function fetchdata(){
+          const resp=await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`);
+          const data= await resp.json();
+          const requireddata= data.meals;
+          setResult(requireddata);
+        }
+        if(!fetchdata){
+           return setLoading("loading.....");
+        }
+        fetchdata();
+      }
+      
+    // const [value , setValue] = useState("")
+    // const [result ,setResult] = useState();
+    // useEffect(() => {
+    //     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`)
+    //       .then((res) => res.json())
+    //       .then((response) => {
+    //           setValue(response);
+    //       });
+    // }, [result]);
     
     return (
-        <div className="search">
-           <input className="input" type="text" placeholder="Search for a meals or keywords" /> 
-           <button><FaSistrix/></button>
-           <button><FaRandom/></button>
+        <div className="main">
+            <div className="search">
+                <input className="input" type="text" 
+                placeholder="Search for a meals or keywords" 
+                value={value} 
+                onChange={(e)=>{
+                    setValue((e.target.value));
+                }}/> 
+                <button onClick={fet()}>
+                    <FaSistrix/>
+                    </button>
+                <button><FaRandom/></button>
+            </div>
+            <div className="display">
+                {result && result.map((ele,index)=><Image key={index} ele={ele}/> )}
+            </div>
         </div>
+        
+        
     )
 }
